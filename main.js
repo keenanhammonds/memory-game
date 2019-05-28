13a688e73ba828d5ffa7c0f8081e4d6309679acb
keenanhammonds <keenan.hammonds301@gmail.com>
2019-05-27 17:56:37 -0700

    Wrapped JS in function scope

--- main.js
+++ main.js
@@ -1,132 +1,133 @@
-console.log('up and running')
+(function(){
+    console.log('up and running')
 
-const cards = [
-    {
-        rank: 'queen',
-        suit: 'hearts',
-        cardImg: "images/queen-of-hearts.png"
-    },
-    {
-        rank: 'queen',
-        suit: 'diamonds',
-        cardImg: "images/queen-of-diamonds.png"
-    },
-    {
-        rank: 'king',
-        suit: 'hearts',
-        cardImg: "images/king-of-hearts.png"
-    },
-    {
-        rank: 'king',
-        suit: 'diamonds',
-        cardImg: "images/king-of-diamonds.png"
-    }
-];
+    const cards = [
+        {
+            rank: 'queen',
+            suit: 'hearts',
+            cardImg: "images/queen-of-hearts.png"
+        },
+        {
+            rank: 'queen',
+            suit: 'diamonds',
+            cardImg: "images/queen-of-diamonds.png"
+        },
+        {
+            rank: 'king',
+            suit: 'hearts',
+            cardImg: "images/king-of-hearts.png"
+        },
+        {
+            rank: 'king',
+            suit: 'diamonds',
+            cardImg: "images/king-of-diamonds.png"
+        }
+    ];
 
-var cardsInPlay = [];
+    var cardsInPlay = [];
 
-let scoreValue = 0;
+    let scoreValue = 0;
 
-let score = document.getElementById("score");
+    let score = document.getElementById("score");
 
-function scoreUpdater(){
+    function scoreUpdater(){
 
-    if(checkForMatch){
-        scoreValue++;
-        score.textContent = scoreValue;
-        console.log(scoreValue);
-    }
-}
-
-function checkForMatch(){
-    if(cardsInPlay.length === 2){
-        if(cardsInPlay[0] === cardsInPlay[1]){
-            alert('you found a match!');
-            console.log(true);
-            scoreUpdater();   
-            return true;
-
-        } else {
-            alert('sorry, try again');
-            console.log(false);
-            return false;
+        if(checkForMatch){
+            scoreValue++;
+            score.textContent = scoreValue;
+            console.log(scoreValue);
         }
     }
-}
 
+    function checkForMatch(){
+        if(cardsInPlay.length === 2){
+            if(cardsInPlay[0] === cardsInPlay[1]){
+                alert('you found a match!');
+                console.log(true);
+                scoreUpdater();   
+                return true;
 
-
-function flipCard(){
-    let cardId = this.getAttribute('data-id');
-    this.setAttribute('src', cards[cardId].cardImg);
-    cardsInPlay.push(cards[cardId].rank);
-    checkForMatch();
-}
-
-let cardTable = document.getElementById('game-board');
-
-function createBoard(){
-    for (let i = 0; i < cards.length; i++){
-        let cardElement = document.createElement('img');
-        cardElement.setAttribute('src', 'images/back.png');
-        cardElement.setAttribute('data-id', i);
-        cardElement.setAttribute('id', "card");
-        cardElement.addEventListener('click', flipCard);
-        document.getElementById('game-board').appendChild(cardElement);
+            } else {
+                alert('sorry, try again');
+                console.log(false);
+                return false;
+            }
+        }
     }
-}
 
 
-function clearBoard(){
-    while(cardTable.firstChild){
-        cardTable.removeChild(cardTable.firstChild);
+
+    function flipCard(){
+        let cardId = this.getAttribute('data-id');
+        this.setAttribute('src', cards[cardId].cardImg);
+        cardsInPlay.push(cards[cardId].rank);
+        checkForMatch();
     }
-    cardsInPlay.length = 0;
-    shuffle(cards)
-}
 
-function clearCardsInPlay(){
-    console.log(cardsInPlay);
-    scoreValue = 0;
-    score.textContent = scoreValue;
-    return cardsInPlay.length = 0;
+    let cardTable = document.getElementById('game-board');
 
-}
-
-function shuffle(array){
-    var currentIndex = array.length;
-    var tempValue;
-    var randomIndex;
-
-    while(0 !== currentIndex){
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        tempValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = tempValue;
+    function createBoard(){
+        for (let i = 0; i < cards.length; i++){
+            let cardElement = document.createElement('img');
+            cardElement.setAttribute('src', 'images/back.png');
+            cardElement.setAttribute('data-id', i);
+            cardElement.setAttribute('id', "card");
+            cardElement.addEventListener('click', flipCard);
+            document.getElementById('game-board').appendChild(cardElement);
+        }
     }
-     return array;
-}
 
-function reset(){
-    let resetButton = document.getElementById("resetButton");
-    resetButton.addEventListener('click', clearBoard);
-    resetButton.addEventListener('click', createBoard);
-}
 
-function startOver(){
-    let startOver = document.getElementById("startOver");
-    startOver.addEventListener("click", clearBoard);
-    startOver.addEventListener("click", clearCardsInPlay);
-    startOver.addEventListener("click", createBoard);
-    startOver.addEventListener("click", console.log("starting over"));
-}
+    function clearBoard(){
+        while(cardTable.firstChild){
+            cardTable.removeChild(cardTable.firstChild);
+        }
+        cardsInPlay.length = 0;
+        shuffle(cards)
+    }
 
-createBoard();
+    function clearCardsInPlay(){
+        console.log(cardsInPlay);
+        scoreValue = 0;
+        score.textContent = scoreValue;
+        return cardsInPlay.length = 0;
 
-reset();
+    }
 
-startOver();
+    function shuffle(array){
+        var currentIndex = array.length;
+        var tempValue;
+        var randomIndex;
 
- 
+        while(0 !== currentIndex){
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            tempValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = tempValue;
+        }
+        return array;
+    }
+
+    function reset(){
+        let resetButton = document.getElementById("resetButton");
+        resetButton.addEventListener('click', clearBoard);
+        resetButton.addEventListener('click', createBoard);
+    }
+
+    function startOver(){
+        let startOver = document.getElementById("startOver");
+        startOver.addEventListener("click", clearBoard);
+        startOver.addEventListener("click", clearCardsInPlay);
+        startOver.addEventListener("click", createBoard);
+        startOver.addEventListener("click", console.log("starting over"));
+    }
+
+    createBoard();
+
+    reset();
+
+    startOver();
+
+})();
